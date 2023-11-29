69452f90819f87a696788bfcb30265de03a93ea4
Anderson Chen <twan0817@gmail.com>
2023-11-29 15:54:44 +0800

    refactor: remove unused codes

--- addSigner.ts
+++ addSigner.ts
@@ -1,23 +1,13 @@
-import { createWalletClient, createPublicClient, http, encodeAbiParameters, getContract } from 'viem';
+import { createWalletClient, createPublicClient, http } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
 import { optimism } from 'viem/chains';
-import {
-    NobleEd25519Signer,
-} from "@farcaster/hub-nodejs";
-import { hexToBytes } from "@noble/hashes/utils";
 import 'dotenv/config';
-
-import { keyGateWayAbi } from "./ABIs/keyGatewayAbi";
 
 const SIGNER = process.env.SIGNER; // <REQUIRED>
 
 const fid = 194467;
 const fidOwner = "0x33b8287511ac7F003902e83D642Be4603afCd876";
-const keyType = 1;
-const metadataType = 1;
-const newSigner = "0x9D2CEE4c8A3382f66CB2b45ce045b3daFc4Fc1fA";
 const newSignerPubKey = "0x0c4d92e149cc48735b7a72f6cf63eff62609cd09591bf805a4af25f189ae9d76";
-const keyGateway = "0x00000000fc56947c7e7183f8ca4b62398caadf0b";
 
 const SIGNED_KEY_REQUEST_VALIDATOR_EIP_712_DOMAIN = {
     name: "Farcaster SignedKeyRequestValidator",
@@ -31,10 +21,6 @@ const main = async () => {
     const account = privateKeyToAccount(SIGNER);
     const walletClient = createWalletClient({
         account,
-        chain: optimism,
-        transport: http()
-    });
-    const publicClient = createPublicClient({ 
         chain: optimism,
         transport: http()
     });
@@ -65,19 +51,6 @@ const main = async () => {
     console.log("request signer", fidOwner);
     console.log("deadline: ", deadline);
     console.log(`Metadata: [${fid}, "${fidOwner}", "${signedMetadata}", ${deadline}]`);
-
-    // TODO: encode metadata
-    // 根據：https://github.com/farcasterxyz/contracts/blob/main/src/validators/SignedKeyRequestValidator.sol#L32C1-L37C6
-    // struct SignedKeyRequestMetadata {
-    //     uint256 requestFid;
-    //     address requestSigner;
-    //     bytes signature;
-    //     uint256 deadline;
-    // }
-
-    // TODO: Create contract instance
-
-    // TODO: Send tx
 };
 
 main();
